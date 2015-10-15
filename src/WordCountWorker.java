1cfbad03ea750bc830f3bc440597549c33f679da
Michael Robison <michael.robison168@gmail.com>
2015-10-15 04:13:08 -0600

    added some synchronization - improved stability
    still inaccurate

--- src/WordCountWorker.java
+++ src/WordCountWorker.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
@@ -11,16 +12,19 @@ public class WordCountWorker implements Runnable {
     private TreeMap<String, Integer> counts;
     private static int chunkNum = 0;
 
-    WordCountWorker(ArrayList<String> _chunk) {
+    public WordCountWorker(ArrayList<String> _chunk) {
         chunk = _chunk;
         counts = new TreeMap<>();
-        ++chunkNum;
+        //increment the number of these threads created with a static, synchronized function
+        increment();
     }
+
 
     public void run() {
         for (String oneLine : chunk) {
             String[] line = oneLine.split("\\s+");
-            for(String word : line) {
+            for (String word : line) {
+                word.replaceAll("\\W+", "");
                 Integer freq = counts.get(word); //use word=key to get value=freq
                 if (freq == null) { //word doesn't exist
                     counts.put(word, 1);
@@ -29,18 +33,25 @@ public class WordCountWorker implements Runnable {
                 }
             }
         }
+        printChunk();
+    }
 
-        FileOutputStream oFile = null;
-        ObjectOutputStream oChunk = null;
+    public synchronized void printChunk() {
         try {
-            oFile = new FileOutputStream("chunk" + chunkNum + ".txt");
-            oChunk = new ObjectOutputStream(oFile);
-            for (int i = 0; i < counts.size(); ++i) {
-                oChunk.writeObject(counts);
+            BufferedWriter outStream = new BufferedWriter(new FileWriter("chunk" + chunkNum + ".txt"));
+            for (Map.Entry<String, Integer> entry : counts.entrySet()) {
+                outStream.write(entry.getKey() + "\t" + entry.getValue() + "\n");
+
             }
+            outStream.close();
         } catch (IOException e) {
             e.printStackTrace();
+
         }
+    }
+
+    public static synchronized void increment() {
+        ++chunkNum;
     }
 }
 
